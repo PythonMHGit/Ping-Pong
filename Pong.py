1669fa0768463d34dfe22f7c23d3dd3b4fb5ef70
PythonMHGit <88116597+PythonMHGit@users.noreply.github.com>
2021-08-04 19:29:13 -0400

    Pong Update #2
    Added:
     Player

--- Pong.py
+++ Pong.py
@@ -7,41 +7,45 @@ FPS = 60
 
 #Variables
 running = True
+paddle_width = 20
+paddle_height = 80
 
 #Game Window Set Up
 win_width = 700
 win_height = 500
 window = display.set_mode((win_width, win_height))
-window.fill((37, 150, 190))
 
 #Classes
-# class GameSprite(sprite.Sprite):
-#     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-#         super().__init__()
-#         self.image = transform.scale(image.load(player_image), (size_x, size_y))
-#         self.speed = player_speed
-#         self.rect = self.image.get_rect()
-#         self.rect.x = player_x
-#         self.rect.y = player_y
-#     def reset(self):
-#         window.blit(self.image, (self.rect.x, self.rect.y))
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
-# class Player(GameSprite):
-#     def update(self):
-#         keys = key.get_pressed()
-#         if keys[W] and self.rect.x > 0:
-#             self.rect.x -= self.speed
-#         if keys[S] and self.rect.x < win_width - 80:
-#             self.rect.x += self.speed
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - paddle_height:
+            self.rect.y += self.speed
+
+#Creating All Sprites
+player = Player("Paddle.png", 0, 0, paddle_width, paddle_height, 5)
 
 #Game Loop
 while running:
     for e in event.get():
         if e.type == QUIT:
             running = False
+    window.fill((37, 150, 190))
 
-
-    window.blit(background,(0, 0))
-
+    player.update()
+    player.reset()
     display.update()
     clock.tick(FPS)
